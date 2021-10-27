42018df841d39b7cadb5a43181ed1c9f0313388a
BHARATH CHANDRA <bharathchandra541@gmail.com>
2021-10-27 19:52:58 +0530

    configure gatsby-browser.js for wrapping page with layout :hammer:

--- gatsby/src/pages/bestdeals.js
+++ gatsby/src/pages/bestdeals.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import Layout from '../components/Layout'
 
 export default function Bestdeals() {
-  return (
-    <Layout>
-      <div>Best Deals !!</div>
-    </Layout>
-  )
+  return <div>Best Deals !!</div>
 }
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import Layout from '../components/Layout'
 
 export default function IndexPage() {
-  return (
-    <Layout>
-      <div>Hey!i am the homepage!!</div>
-    </Layout>
-  )
+  return <div>Hey!i am the homepage!!</div>
 }
