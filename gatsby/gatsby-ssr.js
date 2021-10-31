f1c7d4183d8e24c01854e2d1f8a06630d470abb5
BHARATH CHANDRA <bharathchandra541@gmail.com>
2021-10-31 18:36:47 +0530

    Update ssr config

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Layout from './src/components/Layout'
+import Wrapper from './src/components/Wrapper'
 
 import './src/styles/global.css'
 
 // Wraps every page in a component
 export function wrapPageElement({ element, props }) {
-  return <Layout {...props}>{element}</Layout>
+  return (
+    <Layout>
+      <Wrapper {...props}>{element}</Wrapper>
+    </Layout>
+  )
 }
